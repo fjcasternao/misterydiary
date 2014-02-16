a16447a4b2da50f4dd9e9d11ae0656b58bfec376
Sandra Marchal <sandramarchalsansaloni@gmail.com>
2014-02-16 17:18:38 +0100

    1º version. Panel de pintado de rectangulos

---
+++ src/mistery_diary/Dibujo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mistery_diary;
+import java.awt.Graphics;
+import java.awt.Color;
+
+/**
+ *
+ * @author Sandra
+ */
+public class Dibujo {
+    
+    public void dibujarLinea(Graphics g, int x0, int y0, int x1, int y1){
+        g.drawLine(x0, y0, x1, y1);
+    }
+    
+    public void dibujarCirculo(Graphics g, int x0, int y0, int radio){
+        g.drawOval(x0, y0, radio, radio); // x0 - coordenada x a la izq donde comienza
+                                          // y0 - coordenada y a la izq donde comienza
+    }
+    
+    public void dibujarCuadrado(Graphics g, int x0, int y0, int altura, int anchura){
+        g.setColor(new Color((float)Math.random(),(float)Math.random(),(float)Math.random()));
+        g.fillRect(x0, y0, anchura, altura); // comienza a dibujarlo en la esquina superior izq
+    }
+    
+}
---
+++ src/mistery_diary/Mistery_diary.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mistery_diary;
+
+
+/**
+ *
+ * @author Sandra
+ */
+public class Mistery_diary {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
